186ff3df96c84c5a37a18fab5b177ed77c98f0d1
billk <ikon321@yahoo.com>
2018-07-15 11:25:40 -0700

    add use statement

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use App\Events\RegistrationCompleted;
 use Illuminate\Http\Request;
+use Illuminate\Auth\Events\Registered;
 
 class RegisterController extends Controller
 {
