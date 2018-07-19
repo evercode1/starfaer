a0227217e7b07b3c5fb75548d99e16fd4627c3aa
billk <ikon321@yahoo.com>
2018-07-18 19:26:16 -0700

    book model

--- app/Queries/AllBooksQuery.php
+++ app/Queries/AllBooksQuery.php
@@ -17,6 +17,7 @@ class AllBooksQuery
         $rows = DB::table('books')
             ->select('id',
                 'title',
+                'subtitle',
                 'author',
                 'weight',
                 'url',
