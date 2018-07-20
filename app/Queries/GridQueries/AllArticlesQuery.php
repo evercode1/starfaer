a4635bd82bb458546d46b3bf6b59874d0a145afb
billk <ikon321@yahoo.com>
2018-07-19 18:12:53 -0700

    fixes all-articles query

--- app/Queries/GridQueries/AllArticlesQuery.php
+++ app/Queries/GridQueries/AllArticlesQuery.php
@@ -46,8 +46,8 @@ class AllArticlesQuery implements DataQuery
                           DB::raw('DATE_FORMAT(posts.created_at,
                                   "%m-%d-%Y") as Created'))
                 ->leftJoin('categories', 'category_id', '=', 'categories.id')
-                ->Where('title', 'like', '%' . $keyword . '%')
-                ->orWhere('categories.name', 'like', '%' . $keyword . '%')
+                ->where([['title', 'like', '%' . $keyword . '%'],['is_published', 1]])
+                ->orWhere([['categories.name', 'like', '%' . $keyword . '%'],['is_published', 1]])
                 ->orderBy($column, $direction)
                 ->paginate(5);
 
