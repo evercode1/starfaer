7bd75338c250d377c3cd15f9dba838098a701695
billk <ikon321@yahoo.com>
2018-08-07 11:37:57 -0700

    test

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -39,25 +39,25 @@ class TestController extends Controller
 
 
 
-        $planets = Planet::with('moons')->where('planet_type_id', 3)->get();
+//        $planets = Planet::with('moons')->where('planet_type_id', 3)->get();
+//
+//        foreach ($planets as $planet){
+//
+//            echo $planet->id . ' - ' . $planet->name . ' - ' . $planet->moons->count() . '<br/>';
+//
+//
+//        }
 
-        foreach ($planets as $planet){
 
-            echo $planet->id . ' - ' . $planet->name . ' - ' . $planet->moons->count() . '<br/>';
+        $moons = Moon::where('planet_id', 0)->get();
+
+        foreach($moons as $moon){
+
+            echo $moon->id . ' - ' . $moon->name . '<br/>';
 
 
         }
 
-
-//        $moons = Moon::where('planet_id', 0)->get();
-//
-//        foreach($moons as $moon){
-//
-//            echo $moon->name . '<br/>';
-//
-//
-//        }
-//
 
 
            //return view('test.index');
