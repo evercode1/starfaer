ce9f92cb905d655e8e0e3197725d0c35bda345b3
billk <ikon321@yahoo.com>
2018-08-07 23:26:11 -0700

    fix nav mobile

--- resources/views/layouts/master-partials/footer-guest.blade.php
+++ resources/views/layouts/master-partials/footer-guest.blade.php
@@ -8,4 +8,6 @@
 
 
 <!-- Compiled and minified JavaScript -->
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+<script src="/dist/js/materialize.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.19.2/moment.min.js"></script>
+<script src="/dist/js/init.js"></script>
