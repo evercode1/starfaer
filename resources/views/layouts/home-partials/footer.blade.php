e3cb7e807356adfa506a59ff84316bd3c3ab9112
billk <ikon321@yahoo.com>
2018-08-08 18:28:12 -0700

    footers

--- resources/views/layouts/home-partials/footer.blade.php
+++ resources/views/layouts/home-partials/footer.blade.php
@@ -3,33 +3,31 @@
         <div class="row">
             <div class="col s6 m3">
                 <img class="materialize-logo" src="/dist/img/materialize-logo.png" alt="Materialize">
-                <p>Made with love by Materialize.</p>
+                <p>Made with love</p>
             </div>
             <div class="col s6 m3">
                 <h5>About</h5>
                 <ul>
-                    <li><a href="#!">Blog</a></li>
-                    <li><a href="#!">Pricing</a></li>
-                    <li><a href="#!">Docs</a></li>
+                    <li><a href="/about">Our Story</a></li>
+
                 </ul>
             </div>
             <div class="col s6 m3">
                 <h5>Connect</h5>
                 <ul>
-                    <li><a href="#!">Community</a></li>
-                    <li><a href="#!">Subscribe</a></li>
-                    <li><a href="#!">Email</a></li>
+                    <li><a href="/all-posts">Blog Posts</a></li>
+                    <li><a href="/all-videos">Videos</a></li>
+                    <li><a href="/all-books">books</a></li>
+
                 </ul>
             </div>
             <div class="col s6 m3">
                 <h5>Contact</h5>
                 <ul>
-                    <li><a href="/contact/create">Support</a></li>
-                    <li><a href="#!">Twitter</a></li>
-                    <li><a href="#!">Facebook</a></li>
-                    <li><a href="#!">Github</a></li>
+                    <li><a href="/support-messages">Support</a></li>
+
                 </ul>
             </div>
         </div>
     </div>
-    </footer>
+</footer>
