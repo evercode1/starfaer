e19e7e63bdad6ebb327dc207f93d0a24759c1e26
billk <ikon321@yahoo.com>
2019-11-17 22:41:38 -0800

    fixes user query

--- app/Queries/GridQueries/UserQuery.php
+++ app/Queries/GridQueries/UserQuery.php
@@ -11,7 +11,6 @@ class UserQuery implements DataQuery
     {
         $rows = DB::table('users')
               ->select('id as Id',
-                       'name as Name',
                        'email as Email',
                        'status_id as Status',
                        'is_subscribed as Subscribed',
@@ -28,7 +27,6 @@ class UserQuery implements DataQuery
     {
         $rows = DB::table('users')
                 ->select('id as Id',
-                         'name as Name',
                          'email as Email',
                          'status_id as Status',
                          'is_subscribed as Subscribed',
