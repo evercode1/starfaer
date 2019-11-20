a86aca656caa71affcdc4b09200597cc087be888
billk <ikon321@yahoo.com>
2019-11-20 15:46:18 -0800

    text on log in page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -3,6 +3,12 @@
 @section('content')
 <div class="container">
 
+    <div class="center">
+
+        <h1 class="center flow-text grey-text text-darken-1">Need to Register? <a href="/register">Click Here</a> </h1>
+
+    </div>
+
     <div>
            @include('auth.login-form')
 
