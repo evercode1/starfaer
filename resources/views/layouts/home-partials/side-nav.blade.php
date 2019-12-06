d2285482cc5efbf065e89f4eadac41137abe6220
billk <ikon321@yahoo.com>
2019-12-06 11:21:02 -0800

    left nav

--- resources/views/layouts/home-partials/side-nav.blade.php
+++ resources/views/layouts/home-partials/side-nav.blade.php
@@ -10,31 +10,28 @@
                     <ul>
                         <li><a href="/home" class="waves-effect active">Books<i class="material-icons">star</i></a></li>
                         <li><a href="https://teespring.com/stores/star-faer-merch" class="waves-effect active" target="_blank">Merch<i class="material-icons">star</i></a></li>
+                        <li><a href="/all-videos" class="waves-effect active">Max's Videos<i class="material-icons">star</i></a></li>
                         <li><a href="/worlds" class="waves-effect active">Worlds of Star Faer<i class="material-icons">star</i></a></li>
-                        <li><a href="/all-articles" class="waves-effect active">Articles<i class="material-icons">star</i></a></li>
-                        <li><a href="/all-videos" class="waves-effect active">Videos<i class="material-icons">star</i></a></li>
+                        <li><a href="/all-articles" class="waves-effect active">Max's Megaphone<i class="material-icons">star</i></a></li>
                         <li><a href="/about" class="waves-effect active">About<i class="material-icons">star</i></a></li>
                         <li><a href="/" class="waves-effect active">Splash Page<i class="material-icons">star</i></a></li>
-                        <li><a href="/all-stars" class="waves-effect">All Stars<i class="material-icons">star</i></a></li>
-                        <li><a href="/all-planets" class="waves-effect">All Planets<i class="material-icons">star</i></a></li>
-                        <li><a href="/all-moons" class="waves-effect">All Moons<i class="material-icons">star</i></a></li>
-                        <li><a href="/planets-with-life" class="waves-effect">Planets With Life<i class="material-icons">star</i></a></li>
-                        <li><a href="/life-zones" class="waves-effect">Zones With Life<i class="material-icons">star</i></a></li>
                         </ul>
                 </div>
             </li>
             <li class="bold waves-effect"><a class="collapsible-header">Cosmic Components<i class="material-icons chevron">chevron_left</i></a>
                 <div class="collapsible-body">
                     <ul>
-                        <li><a href="/all-universes" class="waves-effect">All Universes<i class="fa fa-globe"></i></a></li>
+                        <li><a href="/all-stars" class="waves-effect">All Stars<i class="fa fa-star"></i></a></li>
+                        <li><a href="/all-planets" class="waves-effect">All Planets<i class="fa fa-globe"></i></a></li>
+                        <li><a href="/all-moons" class="waves-effect">All Moons<i class="fa fa-globe"></i></a></li>
+                        <li><a href="/planets-with-life" class="waves-effect">Planets With Life<i class="fa fa-globe"></i></a></li>
+                        <li><a href="/life-zones" class="waves-effect">Zones With Life<i class="fa fa-star"></i></a></li>
+                        <li><a href="/all-universes" class="waves-effect">All Universes<i class="fa fa-star"></i></a></li>
                         <li><a href="/all-galaxies" class="waves-effect">All Galaxies<i class="fa fa-star"></i></a></li>
                         <li><a href="/all-zones" class="waves-effect">All Zones<i class="fa fa-star"></i></a></li>
                         <li><a href="/all-star-types" class="waves-effect">All Star Types<i class="fa fa-star"></i></a></li>
-                        <li><a href="/all-stars" class="waves-effect">All Stars<i class="fa fa-star"></i></a></li>
                         <li><a href="/all-planet-types" class="waves-effect">Planet Types<i class="fa fa-globe"></i></a></li>
                         <li><a href="/all-atmospheres" class="waves-effect">All Atmospheres<i class="fa fa-globe"></i></a></li>
-                        <li><a href="/all-planets" class="waves-effect">All Planets<i class="fa fa-globe"></i></a></li>
-                        <li><a href="/all-moons" class="waves-effect">All Moons<i class="fa fa-globe"></i></a></li>
                         <li><a href="/all-surface-types" class="waves-effect">All Surface Types<i class="fa fa-globe"></i></a></li>
 
                     </ul>
